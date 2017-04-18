7cd3095dd23b590fcbf08d41e41671c193b64894
zacharypropert <zacharypropert@gmail.com>
2017-04-18 16:30:07 -0400

    Added try catch block so the weather scrapes the condition properly depending on weather it is during the day or at night. Just need to wait for night time to input the correct selection

--- Weather/src/Weather.java
+++ Weather/src/Weather.java
@@ -21,6 +21,16 @@ public class Weather {
 	private ArrayList<String> t;
 	private ArrayList<String> c;
 	
+	/*
+	 * Scrapes the Location, Temperature and Condition for www.accuweather.com.
+	 * Uses try catch block because at night the class name changes on the website, 
+	 * so the first part tries as if it is day time and if it returns null the catch block
+	 * tries it using the night class name.
+	 * 
+	 * Using arraylists to store the information, still learning how to properly scrape and don't know how to specifically
+	 * get only the one value I'm looking for.
+	 */
+	
 	public void getWeather() throws IOException {
 
 		Document doc = Jsoup.connect("http://www.accuweather.com/").get();
@@ -32,18 +42,22 @@ public class Weather {
 			String tempTemperature = weather.select(".local-temp").text();
 			t.add(tempTemperature);
 			l.add(tempLocation);			
-		}
-		for (Element condition : doc.select("li.bg-s")) {
-			String tempCondition = condition.select(".cond").text();
-			c.add(tempCondition);			
-		}
+		} 
+		
 		location = l.get(1);
 		temperature = t.get(1);
 		try {
-		condition = c.get(0);
+			for (Element condition : doc.select("li.bg-su.cl.hv")) { 
+				String tempCondition = condition.select("div.cond").text();
+				c.add(tempCondition);			
+			}
+			condition = c.get(0);
 		}
 		catch (IndexOutOfBoundsException e){
-			condition = " ";
+			for (Element condition : doc.select("li.bg-su.cl.hv")) {
+				String tempCondition = condition.select("div.cond").text();
+				c.add(tempCondition);			
+			}
 		}
 	}
 }
